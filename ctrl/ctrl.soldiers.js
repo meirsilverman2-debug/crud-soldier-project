7883d0ddada4dce1730afa4707872d8814c9e25d
Meir925man <meirsilverman2@gmail.com>
2026-07-19 14:34:40 +0300

    add: the complet end point that get a soldeir by ID query param stylev

--- ctrl/ctrl.soldiers.js
+++ ctrl/ctrl.soldiers.js
@@ -65,4 +65,3 @@ export async function getSoldierByID(req, res){
 };
 
 
-
--- routes/route.soldeirs.js
+++ routes/route.soldeirs.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { checkBody, checkQuery } from "../middlewhere/middlewhere.soldiers.js";
-import { createSoldier, getSoldeirs } from "../ctrl/ctrl.soldiers.js";
+import { checkBody, checkQuery, checkID } from "../middlewhere/middlewhere.soldiers.js";
+import { createSoldier, getSoldeirs, getSoldierByID } from "../ctrl/ctrl.soldiers.js";
 
 const router = express.Router();
 
 router.post("/", checkBody, createSoldier);
 
-router.get("/", checkQuery, getSoldeirs)
+router.get("/", checkQuery, getSoldeirs);
 
-
+router.get("/:id", checkID, getSoldierByID);
 
 export default router;
 
